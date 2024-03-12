a73946409db9ef08c76e2754b931531cd9a5de69
anukultyagi <anukultyagi321@gmail.com>
2024-03-12 23:39:31 +0530

    added initial styles

--- 06randomQuoteGenerator/tailwind.config.js
+++ 06randomQuoteGenerator/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'background-image': "url('./src/assets/background-image.jpg')",
+
+      }
+    },
   },
   plugins: [],
 }
